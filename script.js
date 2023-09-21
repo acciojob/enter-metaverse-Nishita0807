af3b7a46fb6dc560cc991b6e2e0bb8b50d0a6871
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-21 10:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your JS code here. If required.
+const main = document.body;
+const p = document.createElement("p");
+p.innerText = "Enter the Metaverse";
+p.id = "status";
+const btn = document.createElement("button");
+btn.id = "enterBtn";
+btn.innerText = "Enter";
+main.append(p, btn);
+
+function replaceWithH1(element) {
+  const h1 = document.createElement("h1");
+  h1.innerText = "Entered Metaverse";
+  element.replaceWith(h1);
+}
+
+const button = document.getElementById("enterBtn");
+button.addEventListener("click", function () {
+  replaceWithH1(p);
+});
