d3b9a1c52f8bae2a66f7d42154b16a48b327b916
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-28 17:04:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
- const main = document.body;
-        const p = document.createElement("p");
-        p.innerText = "Enter the Metaverse";
-        p.id = "status";
-        const btn = document.createElement("button");
-        btn.id = "enterBtn";
-        btn.innerText = "Enter";
-        main.append(p, btn);
+ const main=document.body;
+const p=document.createElement("p");
+p.innerText="Enter the Metaverse";
+p.id="status";
+const button=document.createElement("button");
+button.id="enterBtn";
+button.innerText="Enter";
+main.append(p,button);
 
-        function replaceWithH1(element) {
-            const h1 = document.createElement("h1");
-            h1.innerText = "Entered Metaverse";
+function changing(){
+	const para=document.getElementById("status");
+	para.remove();
+	const h1=document.createElement("h1");
+	h1.innerText="Entered Metaverse";
+	main.append(h1,button);
+	
+}
+const clickbtn=document.getElementById("enterBtn");
+enterBtn.onclick=changing;
 
-            // Replace the content of the <p> element with the <h1> element
-            element.innerHTML = '';
-            element.appendChild(h1);
-        }
 
-        const button = document.getElementById("enterBtn");
-        const para = document.getElementById("status");
-        button.addEventListener("click", function () {
-            replaceWithH1(para);
-        });
+
